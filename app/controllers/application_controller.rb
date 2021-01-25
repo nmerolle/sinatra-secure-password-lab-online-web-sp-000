f97df42ad7b06a8beecfc02bb21cf4a5686d7669
nmerolle <nmerolle11@hotmail.com>
2021-01-25 03:20:49 -0500

    commit tree 128b601203508f41e5e2f0ac0794a5336ab283ab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,50 +8,59 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "password_security"
   end
 
-  get "/" do
-    erb :index
-  end
+	get "/" do
+		erb :index
+	end
 
-  get "/signup" do
-    erb :signup
-  end
+	get "/signup" do
+		erb :signup
+	end
 
-  post "/signup" do
-    #your code here
-
-  end
-
-  get '/account' do
-    @user = User.find(session[:user_id])
-    erb :account
-  end
-
-
-  get "/login" do
-    erb :login
-  end
-
-  post "/login" do
-    ##your code here
-  end
-
-  get "/failure" do
-    erb :failure
-  end
-
-  get "/logout" do
-    session.clear
-    redirect "/"
-  end
-
-  helpers do
-    def logged_in?
-      !!session[:user_id]
+	post "/signup" do
+	  user = User.new(:username => params[:username], :password => params[:password])
+ 
+    if params[:username]==""||params[:password]==""
+      redirect "/failure"
+    else
+      redirect "/login"
     end
+	end
 
-    def current_user
-      User.find(session[:user_id])
-    end
-  end
+	get "/login" do
+		erb :login
+	end
+
+	post "/login" do
+		user = User.find_by(:username => params[:username])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect "/account"
+		else
+			redirect "failure"
+		end
+	end
+
+	get "/account" do
+			erb :account
+	end
+
+	get "/failure" do
+		erb :failure
+	end
+
+	get "/logout" do
+		session.clear
+		redirect "/"
+	end
+
+	helpers do
+		def logged_in?
+			!!session[:user_id]
+		end
+
+		def current_user
+			User.find(session[:user_id])
+		end
+	end
 
 end
